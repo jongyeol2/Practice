1d93d67b4c21eb72c8e8f7ec1fdf17c2d327f1bd
jongyeol2 <114922306+jongyeol2@users.noreply.github.com>
2022-12-05 16:27:39 +0900

    d
    d

--- FastCampus/src/ch03/Student.java
+++ FastCampus/src/ch03/Student.java
@@ -10,9 +10,11 @@ public class Student {
         System.out.println(studentID + "학번 학생의 이름은" + studentName + " 이고, 주소는 " + address + "입니다.");
     }
 
+
     public String getStudentName() {
         return studentName;
     }
+
 
     public void setStudentName(String name) {
         studentName = name;
